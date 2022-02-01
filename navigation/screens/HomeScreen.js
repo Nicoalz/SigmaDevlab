286dcde097f95a7757bfd14d3b3baf852a7c412a
“Noro” <thomas.aubert@edu.devinci.fr>
2022-02-01 14:06:23 +0100

    :sparkles: Update TicketScreen

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,110 +1,108 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react'
-import {  View, ScrollView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import {Card, Title} from 'react-native-paper';
+import { useEffect, useState } from 'react'
+import { View, ScrollView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Card, Title } from 'react-native-paper';
 
 export default function HomeScreen({ navigation }) {
 
     const [isLoading, setIsLoading] = useState(true);
     const [events, setEvents] = useState(null);
-    var axe_title ='Axe';
-    var price ='';
-    var event_title ='';
+    var axe_title = 'Axe';
+    var price = '';
+    var event_title = '';
 
     useEffect(() => {
-        fetch('http://192.168.1.17:1337/events',
-        {
-            method: "GET",
-            headers: {
-                'Accept' : 'Application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(response => {
-            setEvents(response)
-            setIsLoading(false)
-        })
-    },[])
+        fetch('http://192.168.0.45:1337/events',
+            {
+                method: "GET",
+                headers: {
+                    'Accept': 'Application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(response => {
+                setEvents(response)
+                setIsLoading(false)
+            })
+    }, [])
 
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-           
-           
-           
-            <Title style={styles.h1}>Event IIM</Title>  
-            <Title style={styles.h2}>Trier</Title> 
+
+
+
+            <Title style={styles.h1}>Event IIM</Title>
+            <Title style={styles.h2}>Trier</Title>
             <ScrollView showsVerticalScrollIndicator={false}>
 
-            
 
-            {isLoading ? <Text>Loading... </Text> : events.map(event => 
-            
-                {
-                    
-                    if (event.axe =="prepa") {
+
+                {isLoading ? <Text>Loading... </Text> : events.map(event => {
+
+                    if (event.axe == "prepa") {
                         axe_title = "Prépa"
-                        
+
                     }
-                    else if (event.axe =="dw") {
+                    else if (event.axe == "dw") {
                         axe_title = "Axe Développement web"
                     }
-                    else if (event.axe =="crea") {
+                    else if (event.axe == "crea") {
                         axe_title = "Axe Création & Design"
                     }
-                    else if (event.axe =="threed") {
+                    else if (event.axe == "threed") {
                         axe_title = "Axe 3D"
                     }
-                    else if (event.axe =="jv") {
+                    else if (event.axe == "jv") {
                         axe_title = "Axe Jeux Vidéo"
                     }
-                    else if (event.axe =="com") {
+                    else if (event.axe == "com") {
                         axe_title = "Axe CDEB"
                     }
                     else {
                         axe_title = "Axe"
                     }
 
-                    if (event.price =="0") {
+                    if (event.price == "0") {
                         price = "Gratuit"
                     }
                     else {
-                        price = event.price + ' €' 
+                        price = event.price + ' €'
                     }
 
-                    
+
 
                     return (
-                        
+
                         <View style={styles.container}>
                             <Card.Title
                                 title={axe_title}
                                 subtitle={event.location}
                             />
                             <View style={styles.image}>
-                                <TouchableOpacity onPress={()=> navigation.navigate('NestedScreen', {eventname : event.title, eventid : event.id })}>
-                                <View style={styles.image_overlay}></View>
-                                <Image style={styles.image} source={{uri:'http://192.168.1.17:1337' + event.image['url'] }} />
+                                <TouchableOpacity onPress={() => navigation.navigate('NestedScreen', { eventname: event.title, eventid: event.id })}>
+                                    <View style={styles.image_overlay}></View>
+                                    <Image style={styles.image} source={{ uri: 'http://192.168.0.45:1337' + event.image['url'] }} />
                                 </TouchableOpacity>
                             </View>
-                            
+
                             <Title style={styles.absolute}>{event.title}</Title>
                             <Title style={styles.price}>{price}</Title>
-                            
+
 
                         </View>
-                        
+
                     )
                 }
-            
-            
-            
-            
-            )}
-        </ScrollView>
-           
-            
-            
+
+
+
+
+                )}
+            </ScrollView>
+
+
+
 
 
 
@@ -116,14 +114,14 @@ export default function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-    width: 300,
-    margin: 20,
-    flex: 1,
-    alignItems: 'center',
+        width: 300,
+        margin: 20,
+        flex: 1,
+        alignItems: 'center',
     },
 
     h1: {
-        fontFamily:'Roboto',
+        fontFamily: 'Roboto',
         fontSize: 30,
         fontWeight: "bold",
         textAlign: 'center',
@@ -132,20 +130,20 @@ const styles = StyleSheet.create({
 
     h2: {
         fontSize: 20,
-        textAlign:'center',
-        
+        textAlign: 'center',
+
     },
 
     image: {
-        width: '100%', 
+        width: '100%',
         height: 200,
         position: 'relative',
         borderRadius: 10,
     },
 
-    
+
     image_overlay: {
-        backgroundColor : 'rgba(0,0,0,0.45)',
+        backgroundColor: 'rgba(0,0,0,0.45)',
         position: 'absolute',
         width: '100%',
         height: 200,
@@ -155,14 +153,14 @@ const styles = StyleSheet.create({
 
 
     absolute: {
-        color :'white',
+        color: 'white',
         position: 'absolute',
         bottom: 10,
         left: 20,
     },
 
-    price:{
-        color :'white',
+    price: {
+        color: 'white',
         position: 'absolute',
         bottom: 10,
         right: 20,
--- navigation/screens/TicketsScreen.js
+++ navigation/screens/TicketsScreen.js
@@ -1,10 +1,155 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react'
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { Card, Title } from 'react-native-paper';
+
 
 export default function TicketsScreen({ navigation }) {
+    const [usersEvent, setUsersEvent] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://192.168.0.45:1337/users/1',
+            {
+                method: "GET",
+                headers: {
+                    'Accept': 'Application/json'
+                }
+            })
+            .then((response) => {
+
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong with Users');
+                }
+            })
+            .then((responseJson) => {
+                setUsersEvent(responseJson)
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                console.log(error);
+
+            });
+
+    }, []
+    )
+
+
     return (
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Tickets Screen</Text>
+
+            <View showsVerticalScrollIndicator={false}>
+                <Text style={styles.h1}>Mes inscriptions</Text>
+                <Text style={styles.h3}>1 évènement à valider disponible</Text>
+                <View style={{ flex: 1, flexDirection: 'row', marginVertical: 20 }}>
+                    <Text style={[styles.button, styles.not_selected]} nativeID='favourite_button'>Favoris</Text>
+                    <Text style={[styles.button, styles.selected]} nativeID='register_button'>Inscrit</Text>
+                </View>
+
+            </View>
+            <ScrollView>
+                
+                {isLoading ? <Text>Loading Tickets...</Text> :
+                    (usersEvent.registered_events.map(registered => {
+
+                        return (
+
+                            <View style={styles.container}>
+                                <Card.Title
+                                    title={registered.title}
+                                    subtitle={registered.date}
+                                />
+                            </View>
+                        )
+                    }),
+                        usersEvent.favourites.map(favourite => {
+
+                            return (
+
+                                <View style={styles.container}>
+                                    <Card.Title
+                                        title={favourite.title}
+                                        subtitle={favourite.date}
+                                    />
+
+
+                                </View>
+                            )
+
+                        })
+                    )
+
+
+                }
+            </ScrollView>
+            
         </View>
+        
+
+
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        margin: 20,
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    h1: {
+        fontFamily: 'Roboto',
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: 'center',
+        marginTop: 100,
+    },
+    h2: {
+        fontSize: 20,
+        textAlign: 'center',
+
+    },
+    h3: {
+        fontSize: 15,
+        textAlign: 'center',
+
+    },
+
+    button: {
+        fontSize: 20,
+        display: 'flex',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 20,
+        height: 40,
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        borderRadius: 20,
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7
+    },
+
+    selected: {
+        backgroundColor: '#cccbcc',
+
+    },
+    not_selected: {
+        backgroundColor: 'white',
+
+    }
+
+
+
+});
